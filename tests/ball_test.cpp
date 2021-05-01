6223d4450593f5d6bb9418b90b3904290a1e3ad9
tony430 <tonychang430@hotmail.com>
2021-05-01 20:25:19 +0800

    Added game ball collide wall test

--- tests/ball_test.cpp
+++ tests/ball_test.cpp
@@ -8,18 +8,87 @@ TEST_CASE("Game ball movement") {
   glm::vec2 top_left_pixel(100, 100);
   glm::vec2 bottom_right(750, 550);
   glm::vec2 board_position(700, 325);
-  glm::vec2 ball_speed(3, 4);
-  glm::vec2 ball_position(300, 300);
   ballblaster::EnemyBlock enemyBlock(glm::vec2(100, 100));
   std::list<ballblaster::EnemyBlock> enemies;
   enemies.push_back(enemyBlock);
 
   SECTION("Movement after 1 frame without any collision") {
+    glm::vec2 ball_speed(3, 4);
+    glm::vec2 ball_position(300, 300);
     ballblaster::BallBlasterEngine engine(top_left_pixel, bottom_right,
                                           board_position, ball_speed,
                                           ball_position, enemies);
     engine.AdvanceOneFrame();
     REQUIRE(engine.GetGameball().GetPosition().x == 303);
     REQUIRE(engine.GetGameball().GetPosition().y == 304);
+  }
+}
+
+TEST_CASE("Game ball collision with wall") {
+  glm::vec2 top_left_pixel(100, 100);
+  glm::vec2 bottom_right(750, 550);
+  glm::vec2 board_position(700, 325);
+  ballblaster::EnemyBlock enemyBlock(glm::vec2(100, 100));
+  std::list<ballblaster::EnemyBlock> enemies;
+  enemies.push_back(enemyBlock);
+
+  SECTION("Ball collides left wall") {
+    glm::vec2 ball_speed(-4, 2);
+    glm::vec2 ball_position(101, 200);
+    ballblaster::BallBlasterEngine engine(top_left_pixel, bottom_right,
+                                          board_position, ball_speed,
+                                          ball_position, enemies);
+    engine.AdvanceOneFrame();
+    // negates x
+    REQUIRE(engine.GetGameball().GetVelocity().x == 4);
+    REQUIRE(engine.GetGameball().GetVelocity().y == 2);
+  }
+
+  SECTION("Ball collides right wall") {
+    glm::vec2 ball_speed(9, 2);
+    glm::vec2 ball_position(547, 300);
+    ballblaster::BallBlasterEngine engine(top_left_pixel, bottom_right,
+                                          board_position, ball_speed,
+                                          ball_position, enemies);
+    engine.AdvanceOneFrame();
+    // negates x
+    REQUIRE(engine.GetGameball().GetVelocity().x == -9);
+    REQUIRE(engine.GetGameball().GetVelocity().y == 2);
+  }
+
+  SECTION("Ball collides top wall") {
+    glm::vec2 ball_speed(5, -7);
+    glm::vec2 ball_position(150, 103);
+    ballblaster::BallBlasterEngine engine(top_left_pixel, bottom_right,
+                                          board_position, ball_speed,
+                                          ball_position, enemies);
+    engine.AdvanceOneFrame();
+    REQUIRE(engine.GetGameball().GetVelocity().x == 5);
+    // negates y
+    REQUIRE(engine.GetGameball().GetVelocity().y == 7);
+  }
+
+  SECTION("Ball collides top left corner") {
+    glm::vec2 ball_speed(-5, -7);
+    glm::vec2 ball_position(102, 103);
+    ballblaster::BallBlasterEngine engine(top_left_pixel, bottom_right,
+                                          board_position, ball_speed,
+                                          ball_position, enemies);
+    engine.AdvanceOneFrame();
+    // negates x and y
+    REQUIRE(engine.GetGameball().GetVelocity().x == 5);
+    REQUIRE(engine.GetGameball().GetVelocity().y == 7);
+  }
+
+  SECTION("Ball collides top right corner") {
+    glm::vec2 ball_speed(5, -7);
+    glm::vec2 ball_position(547, 103);
+    ballblaster::BallBlasterEngine engine(top_left_pixel, bottom_right,
+                                          board_position, ball_speed,
+                                          ball_position, enemies);
+    engine.AdvanceOneFrame();
+    // negates x and y
+    REQUIRE(engine.GetGameball().GetVelocity().x == -5);
+    REQUIRE(engine.GetGameball().GetVelocity().y == 7);
   }
 }
